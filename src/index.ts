14ef65588b1b08c217626fee3eb618d10742cff8
bruno <bruno.salzano@unicaspa.it>
2024-01-07 12:53:43 +0100

    Added EXPRESS_HOST setting Cleaned up some code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import * as dotenv from "dotenv";
 import { logger } from "./lib/Logger";
 
 dotenv.config();
-const port = process.env.EXPRESS_PORT || 3000;
+const port = parseInt(process.env.EXPRESS_PORT || "3000");
+const hostname = process.env.EXPRESS_HOST || "localhost";
+
 process.on("uncaughtException", (err) => {
   logger.error(`unhandled exception (kill) message: ${err.message}`);
   logger.error(`unhandled exception (kill) stack: ${err.stack}`);
@@ -13,15 +15,11 @@ process.on("uncaughtException", (err) => {
 const app = new App();
 
 App.openDatasources().then(() => {
-  app.express.listen(port, (err: string): void => {
-    if (err) {
-      logger.error("error %s", err);
-    }
-    logger.info("Listening on port %d", port);
+  app.express.listen(port, hostname, () => {
+    logger.info(`Listening on ${hostname}:${port}`);
   });
 })
-  .catch(async (err) => {
-    logger.error("error %s", err);
+  .catch(async () => {
     await App.closeDatasource();
     process.exit(1);
   });
